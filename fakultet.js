e172347ed016b063fe9b314554be2877c3d9c69e
07Siddi <07sidneyly@gmail.com>
2025-03-31 12:39:06 +0200

    test

--- fakultet.js
+++ fakultet.js
@@ -1,10 +1,10 @@
 export function fakultet(tall) {
-    if (tall <= 0) return 1;
+    if (tall <= 0) return 0;
     let result = 1;
     for (let i = 1; i <= tall; i++) {
         result *= i;
     }
-    return result;
+    return result; 
 }
 
 
